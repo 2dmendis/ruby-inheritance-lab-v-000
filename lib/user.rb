32c420df7f668a808cebd055577a19bbf235202b
Dmitri Mendis <2dmendis@gmail.com>
2018-06-04 19:22:25 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 class User 
   attr_accessor :first_name, :last_name
-
+  
+  KNOWLEDGE = ["I hope this works", "Hopefully deleting everything didnt mess this lab up", "Please work", "Last element in this array"]
 end 
